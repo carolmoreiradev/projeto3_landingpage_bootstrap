ab77c819158fe21384c80a07ea044b4faec45086
carolmoreiradev <carolmoreira.dev@gmail.com>
2025-02-21 22:50:15 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+//aplicação de máscara de telefone
+
+    var options =  {
+        onChange: function(tel){
+            let masks = ['(00) 0000-0000 0', '(00) 00000-0000'];
+            let mask = (tel.length>14) ? masks[1] : masks[0];
+        
+            $('#telefone').mask(mask, options);  
+        }
+    };
+
+    $('#telefone').mask('(00) 0000-0000 0', options)
+
+
+    $('form').validate({
+        rules:{
+            nome:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            telefone:{
+                required: false
+            },
+            mensagem:{
+                required: true
+            },
+        },
+        messages:{
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos > 1){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }   else {
+                alert(`Existe ${camposIncorretos} campo incorreto`)
+            }
+        }
+    })
+
+})
